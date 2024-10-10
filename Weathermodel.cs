5301f233b44bbf017d119b875390de4e81738411
Sllivy <144355273+Sllivy@users.noreply.github.com>
2024-10-10 12:42:15 -0400

    Update Weathermodel.cs

--- Weathermodel.cs
+++ Weathermodel.cs
@@ -1,1 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace EwokWeatherRescueApp.Models
+{
+    public class WeatherModel
+    {
+        public string Location { get; set; }
+        public string Condition { get; set; }
+        public string HighTemperature { get; set; }
+        public string LowTemperature { get; set; }
+        public string CurrentTemperature { get; set; }
+    }
+
+}
